81e57094ad6640ff587ab33011d1302cd85ec768
John Patrick Villalon <jocvillalon@yahoo.com.ph>
2026-08-17 12:41:20 +0800

    Feat: Adds events for OnCreateBullet and OnCreateExplosions

--- GodotWildJam96.Game/globals/EventBus.cs
+++ GodotWildJam96.Game/globals/EventBus.cs
@@ -7,20 +7,26 @@ public partial class EventBus : Node
 {
 
     public static EventBus Instance { get; private set; }
+
+    // Sun events
     public event Action<Node2D, SunInteractionArea> OnShipEntered;
     public event Action<Node2D, SunInteractionArea> OnShipExited;
     public event Action<SunInteractionArea> OnSiphonStart;
     public event Action<SunInteractionArea> OnSiphonEnd;
+
+    // Shooting events
+    public event Action<Vector2, Vector2, float, PackedScene> OnCreateBullet;
+    public event Action<Vector2> OnCreateExplosion; // Usually for enemy death
+
     public override void _Ready()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
+        if (Instance is not null)
         {
             QueueFree();
+            return;
         }
+
+        Instance = this;
     }
 
     public void EmitOnShipEntered(Node2D ship, SunInteractionArea interactionArea)
@@ -44,5 +50,17 @@ public partial class EventBus : Node
     {
         GD.Print($"Siphon End Event Emitted {interactionArea.Name} on EventBus {GetInstanceId()}, subscribers: {OnSiphonEnd?.GetInvocationList().Length ?? 0}");
         OnSiphonEnd?.Invoke(interactionArea);
+    }
+
+    public static void EmitOnCreateBullet(Vector2 position, Vector2 direction, float speed, PackedScene scene)
+    {
+        GD.Print("Im firing ma lazors");
+        Instance.OnCreateBullet?.Invoke(position, direction, speed, scene);
+    }
+
+    public static void EmitOnCreateExplosion(Vector2 position)
+    {
+        GD.Print("Explosion triggered");
+        Instance.OnCreateExplosion?.Invoke(position);
     }
 }
