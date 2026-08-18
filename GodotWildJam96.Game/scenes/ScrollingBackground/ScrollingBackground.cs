917d19880e06221e7958ddbcf9d62ecc0afec2e9
John Patrick Villalon <jocvillalon@yahoo.com.ph>
2026-08-18 20:17:53 +0800

    Feat: Adds logic for scrolling backgrounds

--- GodotWildJam96.Game/scenes/ScrollingBackground/ScrollingBackground.cs
+++ GodotWildJam96.Game/scenes/ScrollingBackground/ScrollingBackground.cs
@@ -12,5 +12,38 @@ public partial class ScrollingBackground : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        if (_scrollingImages is null) QueueFree();
+        Scale = new Vector2(_targetScale, _targetScale);
+        Position -= new Vector2(0f, _baseSize.Y * _targetScale);
+    }
+
+    private void AddLayer(float currentScrollScale, Texture2D image)
+    {
+        Parallax2D parallax2D = new()
+        {
+            RepeatSize = new(_baseSize.X, 0.0f),
+            ScrollScale = new(currentScrollScale, 1.0f),
+        };
+
+        Sprite2D sprite = new()
+        {
+            Texture = image,
+            Centered = false,
+        };
+
+        parallax2D.AddChild(sprite);
+        AddChild(parallax2D);
+    }
+
+    private void Setup()
+    {
+        float scrollGap = 1.0f / _scrollingImages.Images.Count;
+        float currentScrollScale = 0.0f;
+
+        for (int i = 0; i < _scrollingImages.Images.Count; ++i)
+        {
+            AddLayer(currentScrollScale, _scrollingImages.Images[i]);
+            currentScrollScale += scrollGap;
+        }
     }
 }
