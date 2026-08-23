99cdf2e0b745297670e1d6a566d498f20482430f
John Patrick Villalon <jocvillalon@yahoo.com.ph>
2026-08-24 00:47:32 +0800

    Fix: Sun interaction area scales with energy instead of collapsing

--- GodotWildJam96.Game/scenes/MainSun/MainSun.cs
+++ GodotWildJam96.Game/scenes/MainSun/MainSun.cs
@@ -1,19 +1,22 @@
+using System;
 using Godot;
 using GodotWildJam96;
-using System;
 
 public partial class MainSun : Sun
 {
 
     [Export] private PackedScene _gameOverScene;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    protected override float InteractionAreaScaleMultiplier => 4.0f;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
     {
         base._Ready();
         _maxEnergy = 15;
         _currentEnergy = 3;
         _energyValuebar.InitializeValues(_maxEnergy, _currentEnergy);
+        UpdateInteractionAreaScale();
     }
 
     public override void _ExitTree()
@@ -21,9 +24,9 @@ public partial class MainSun : Sun
         base._ExitTree();
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
         if (_currentEnergy == 0)
         {
             GameOver();
@@ -32,7 +35,7 @@ public partial class MainSun : Sun
         {
             WinTheGame();
         }
-	}
+    }
 
     private void GameOver()
     {
--- GodotWildJam96.Game/scenes/Sun/Sun.cs
+++ GodotWildJam96.Game/scenes/Sun/Sun.cs
@@ -9,7 +9,6 @@ namespace GodotWildJam96;
 public partial class Sun : Area2D
 {
     //Sun Node Parts
-    private Sprite2D _lightRadiusSprite;
     private SunInteractionArea _mySunInteractionArea;
     [Export] public EnergyBar _energyValuebar;
     [Export] public AudioStreamPlayer2D _siphonSound;
@@ -20,6 +19,10 @@ public partial class Sun : Area2D
     private int _sunLevel = 0;
     public int _maxEnergy;
     public int _currentEnergy;
+
+    // Interaction area never scales below this much energy's worth of size,
+    // so a depleted sun stays reachable instead of shrinking to an uninteractable point.
+    private const float MinInteractionEnergy = 2f;
 
     //Light radius will be dependent on energy level of sun
     private float _lightRadius = 100.0f;
@@ -47,12 +50,12 @@ public partial class Sun : Area2D
         //GD.Print($"[{GetPath()}] Subscribed to EventBus {EventBus.Instance.GetInstanceId()}");
 
         _mySunInteractionArea = GetChild<SunInteractionArea>(0);
-        _lightRadiusSprite = _mySunInteractionArea._lightRadiusSprite;
         //Random range for sun level, this will be used to determine the max energy of the sun
         _sunLevel = GD.RandRange(1, 6);
         _maxEnergy = _sunLevel + 3;
         _currentEnergy = GD.RandRange(3, _maxEnergy);
         _energyValuebar.InitializeValues(_maxEnergy, _currentEnergy);
+        UpdateInteractionAreaScale();
         AddToGroup(GameConstants.GroupSuns);
     }
 
@@ -84,8 +87,8 @@ public partial class Sun : Area2D
             else if (_siphonTimePassed > 1.8f)
             {
                 _siphonSound.Play();
-                _lightRadiusSprite.Scale = new Vector2((_currentEnergy / _maxEnergy), (_currentEnergy / _maxEnergy));
                 _currentEnergy -= _sunSiphonRate;
+                UpdateInteractionAreaScale();
                 if (_siphonOwner == 0)
                 {
                     EventBus.EmitOnEnergySiphoned(_sunSiphonRate);
@@ -107,8 +110,8 @@ public partial class Sun : Area2D
             else if (_siphonTimePassed > 1.8f)
             {
                 _siphonSound.Play();
-                _lightRadiusSprite.Scale = new Vector2((_currentEnergy / _maxEnergy), (_currentEnergy / _maxEnergy));
                 _currentEnergy += _sunSiphonRate;
+                UpdateInteractionAreaScale();
                 _energyValuebar.UpdateValue(_currentEnergy);
                 _siphonTimePassed = 0.0f;
                 _siphonCount++;
@@ -119,6 +122,18 @@ public partial class Sun : Area2D
         {
             _siphonSound.PitchScale = 1.0f + (0.15f * _siphonCount);
         }
+    }
+
+    // Subclasses (MainSun) can scale their interaction area up independently of the energy ratio.
+    protected virtual float InteractionAreaScaleMultiplier => 1f;
+
+    // Scales the whole interaction area (its collision radius and its light-glow sprite
+    // together, since both are children of it) to match the sun's current energy ratio.
+    protected void UpdateInteractionAreaScale()
+    {
+        float energyRatio = Mathf.Max(_currentEnergy, MinInteractionEnergy) / _maxEnergy;
+        float scale = energyRatio * InteractionAreaScaleMultiplier;
+        _mySunInteractionArea.Scale = new Vector2(scale, scale);
     }
 
     public void OnPlayerExited(Player player, SunInteractionArea interactionArea)
